38352e293dad915791712acfcccff40c1a9361b7
Aliaksandr Kananenka <Aliaksandr_Kananenka@epam.com>
2019-12-23 15:49:31 +0300

    Add public path.

--- root/src/core/root-component.js
+++ root/src/core/root-component.js
@@ -5,11 +5,9 @@ import { FilmListContainer } from "../features/film/film-list";
 import { MovieDetailContainer } from "../features/film/film-detail";
 import { InvalidUrlComponent } from "../features/film/shared/components";
 
-import config from "../../../config";
-
 const Root = () => {
     return (
-        <Router path={config.PUBLIC_URL + '/'}>
+        <Router path={process.env.PUBLIC_URL + '/'}>
             <Switch>
                 <Route exact path = "/" component = {FilmListContainer} />
                 <Route exact path = "/search" component = {FilmListContainer} />
