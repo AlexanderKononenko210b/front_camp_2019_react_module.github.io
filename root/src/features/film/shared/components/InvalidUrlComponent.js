ecf106a1aa272ab61ea0a5f986e97e70713f30af
Aliaksandr Kananenka <Aliaksandr_Kananenka@epam.com>
2019-12-23 14:53:54 +0300

    Fix error with 404 page.

--- root/src/features/film/shared/components/InvalidUrlComponent.js
+++ root/src/features/film/shared/components/InvalidUrlComponent.js
@@ -3,7 +3,7 @@ import React from "react";
 const InvalidUrlComponent = (props) => {
     return (
         <div id="main">
-            <div class="fof">
+            <div className="fof">
                     <h1>Error 404</h1>
             </div>
         </div>
