3e248dceaeca09007024c25031ea75ce9a212276
Aliaksandr Kananenka <Aliaksandr_Kananenka@epam.com>
2019-12-23 15:37:09 +0300

    Change path for router.

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const config = {
     API_END_POINT: "https://reactjs-cdp.herokuapp.com/",
-    PATH_DEFAULT: "movies"
+    PATH_DEFAULT: "movies",
+    PUBLIC_URL: "https://github.com/AlexanderKononenko210b/front_camp_2019_react_module.github.io"
 }
 
 export default config;
--- root/src/core/root-component.js
+++ root/src/core/root-component.js
@@ -5,9 +5,11 @@ import { FilmListContainer } from "../features/film/film-list";
 import { MovieDetailContainer } from "../features/film/film-detail";
 import { InvalidUrlComponent } from "../features/film/shared/components";
 
+import config from "../../../config";
+
 const Root = () => {
     return (
-        <Router>
+        <Router path={config.PUBLIC_URL + '/'}>
             <Switch>
                 <Route exact path = "/" component = {FilmListContainer} />
                 <Route exact path = "/search" component = {FilmListContainer} />
